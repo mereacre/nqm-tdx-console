b6a7870b03b4e8e8b3599a712a576bfac40cab8a
Alex <mereacre@gmail.com>
2020-01-23 15:23:47 +0000

    corrected jest module function test

--- src/connect.js
+++ src/connect.js
@@ -1,38 +1,36 @@
-module.exports = function(envConfig, tdxConfigs) {
-  "use strict";
+"use strict";
 
-  const utils = require("./utils");
-  const TDXApi = require("@nqminds/nqm-api-tdx");
+const utils = require("./utils");
+const TDXApi = require("@nqminds/nqm-api-tdx");
 
-  function connectWithToken(config, token) {
-    config.accessToken = token;
-    return new TDXApi(config);
-  }
+function connectWithToken(config, token) {
+  config.accessToken = token;
+  return new TDXApi(config);
+}
 
-  async function connectWithSecret(config, secret) {
-    const api = new TDXApi(config);
-    await api.authenticate(secret.id, secret.secret);
-    return api;
-  }
+async function connectWithSecret(config, secret) {
+  const api = new TDXApi(config);
+  await api.authenticate(secret.id, secret.secret);
+  return api;
+}
 
-  async function connect(alias) {
-    const aliasUpper = alias.toUpperCase();
-    const tdxKeys = utils.getTdxKeys(envConfig);
-    const tdxTokens = utils.getTdxTokens(tdxKeys);
-    const tdxSecrets = utils.getTdxSecrets(tdxKeys);
+async function connect({envConfig, tdxConfigs, alias}) {
+  const aliasUpper = alias.toUpperCase();
+  const tdxKeys = utils.getTdxKeys(envConfig);
+  const tdxTokens = utils.getTdxTokens(tdxKeys);
+  const tdxSecrets = utils.getTdxSecrets(tdxKeys);
 
-    const config = tdxConfigs[aliasUpper];
+  const config = tdxConfigs[aliasUpper];
 
-    if (aliasUpper in tdxTokens) {
-      return connectWithToken(config, tdxTokens[aliasUpper]);
-    } else if (aliasUpper in tdxSecrets) {
-      return connectWithSecret(config, tdxSecrets[aliasUpper]);
-    } else throw Error("No tdx credentials present!");
-  }
+  if (aliasUpper in tdxTokens) {
+    return this.connectWithToken(config, tdxTokens[aliasUpper]);
+  } else if (aliasUpper in tdxSecrets) {
+    return this.connectWithSecret(config, tdxSecrets[aliasUpper]);
+  } else throw Error("No tdx credentials present!");
+}
 
-  return {
-    connect,
-    connectWithToken,
-    connectWithSecret,
-  };
+module.exports = {
+  connectWithToken,
+  connectWithSecret,
+  connect,
 };
--- tests/connect.test.js
+++ tests/connect.test.js
@@ -1,5 +1,5 @@
+const connect = require("../src/connect");
 const TDXApi = require("@nqminds/nqm-api-tdx");
-const connect = require("../src/connect")({}, {});
 
 const mockAuthenticate = jest.fn();
 jest.mock("@nqminds/nqm-api-tdx", () => {
@@ -24,4 +24,20 @@ test("connectWithSecret should call the authenticate function", async() => {
   await connect.connectWithSecret({}, {id: "", secret: ""});
   expect(TDXApi).toHaveBeenCalledTimes(1);
   expect(mockAuthenticate.mock.calls[0][0]).toEqual("");
+});
+
+test("connect should call connectWithToken function for a token env", async() => {
+  const spy = jest.spyOn(connect, "connectWithToken");
+  connect.connect({
+    envConfig: {
+      "TDX_TOKEN_ALIAS": "12345",
+    },
+    tdxConfigs: {
+      "ALIAS": {},
+    },
+    alias: "ALIAS",
+  });
+
+  expect(spy).toHaveBeenCalled();
+  spy.mockRestore();
 });
