0b7000f7c114e02a09062788194993a93529f2f5
Alex <mereacre@gmail.com>
2020-01-23 12:38:15 +0000

    Added connect method

--- src/connect.js
+++ src/connect.js
@@ -1,12 +1,33 @@
 module.exports = function() {
   "use strict";
+
   const utils = require("./utils");
+  const TDXApi = require("@nqminds/nqm-api-tdx");
 
-  function connect(envConfig) {
+  function connectWithToken(config, token) {
+    config.accessToken = token;
+    return new TDXApi(config);
+  }
+
+  async function connectWithSecret(config, secret) {
+    const api = new TDXApi(config);
+    await api.authenticate(secret.id, secret.secret);
+    return api;
+  }
+
+  async function connect({envConfig, alias, tdxConfigs}) {
+    const aliasUpper = alias.toUpperCase();
     const tdxKeys = utils.getTdxKeys(envConfig);
-
     const tdxTokens = utils.getTdxTokens(tdxKeys);
     const tdxSecrets = utils.getTdxSecrets(tdxKeys);
+
+    const config = tdxConfigs[aliasUpper];
+
+    if (aliasUpper in tdxTokens) {
+      return connectWithToken(config, tdxTokens[aliasUpper]);
+    } else if (aliasUpper in tdxSecrets) {
+      return connectWithSecret(config, tdxSecrets[aliasUpper]);
+    } else throw Error("No tdx credentials present!");
   }
 
   return {
